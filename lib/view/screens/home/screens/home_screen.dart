f993e6d785c95a764645d50f0f13351c9b5242fc
Zain Ahmad <101584099+zainahmad0580@users.noreply.github.com>
2024-02-06 12:32:11 +0500

    movie list screen in home screen

--- lib/view/screens/home/screens/home_screen.dart
+++ lib/view/screens/home/screens/home_screen.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:movieapp/api/movie_api.dart';
-import 'package:movieapp/model/movie_model.dart';
 import 'package:movieapp/provider/navbar_provider.dart';
 import 'package:movieapp/view/screens/dashboard/dashboard_screen.dart';
 import 'package:movieapp/view/screens/home/widgets/home_app_bar.dart';
 import 'package:movieapp/view/screens/media_library/media_library_screen.dart';
 import 'package:movieapp/view/screens/more/more_screen.dart';
-import 'package:movieapp/view/screens/movie/widgets/movie_card.dart';
+import 'package:movieapp/view/screens/movie/screens/movie_list_screen.dart';
 import 'package:movieapp/view/widgets/custom_navbar.dart';
-import 'package:movieapp/view/widgets/shimmer_card_effect.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -24,43 +21,11 @@ class HomeScreen extends StatelessWidget {
           padding: EdgeInsets.symmetric(
               horizontal: size.width * 0.03, vertical: size.height * 0.03),
           child: Consumer<NavBarProvider>(builder: (context, value, child) {
-            return IndexedStack(index: value.index, children: [
-              const DashboardScreen(),
-              RefreshIndicator(
-                onRefresh: () async => await MovieApi.getAllMovies(),
-                child: ListView(children: [
-                  FutureBuilder(
-                      future: MovieApi.getAllMovies(),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return ShimmerCardEffect(
-                              items: 4,
-                              height: size.height * 0.25,
-                              width: size.width * 0.9);
-                        } else if (snapshot.hasError) {
-                          return const Icon(Icons.error);
-                        } else {
-                          List<MovieModel> movies = snapshot.data ?? [];
-
-                          if (movies.isEmpty) {
-                            return const Text('No movie found');
-                          }
-
-                          return ListView.builder(
-                              physics: const BouncingScrollPhysics(),
-                              shrinkWrap: true,
-                              itemCount: movies.length,
-                              itemBuilder: ((context, index) {
-                                final movie = movies[index];
-                                return MovieCard(movieModel: movie);
-                              }));
-                        }
-                      })
-                ]),
-              ),
-              const MediaLibraryScreen(),
-              const MoreScreen()
+            return IndexedStack(index: value.index, children: const [
+              DashboardScreen(),
+              MovieListScreen(),
+              MediaLibraryScreen(),
+              MoreScreen()
             ]);
           }),
         ));
--- lib/view/screens/movie/screens/movie_list_screen.dart
+++ lib/view/screens/movie/screens/movie_list_screen.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:movieapp/api/movie_api.dart';
+import 'package:movieapp/model/movie_model.dart';
+import 'package:movieapp/view/screens/movie/widgets/movie_card.dart';
+import 'package:movieapp/view/widgets/shimmer_card_effect.dart';
 
 class MovieListScreen extends StatelessWidget {
   const MovieListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var size = MediaQuery.of(context).size;
+    return RefreshIndicator(
+      onRefresh: () async => await MovieApi.getAllMovies(),
+      child: ListView(children: [
+        FutureBuilder(
+            future: MovieApi.getAllMovies(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return ShimmerCardEffect(
+                    items: 4,
+                    height: size.height * 0.25,
+                    width: size.width * 0.9);
+              } else if (snapshot.hasError) {
+                return const Icon(Icons.error);
+              } else {
+                List<MovieModel> movies = snapshot.data ?? [];
+
+                if (movies.isEmpty) {
+                  return const Text('No movie found');
+                }
+
+                return ListView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: movies.length,
+                    itemBuilder: ((context, index) {
+                      final movie = movies[index];
+                      return MovieCard(movieModel: movie);
+                    }));
+              }
+            })
+      ]),
+    );
   }
 }
